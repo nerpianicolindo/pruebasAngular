e05e3faef4ef86f6761fe9108f29ec64b85b278b
algonzalez <algonzalez@movisat.com>
2021-05-21 08:34:22 +0200

    Api calls

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,42 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  data: any;
 
-  constructor() { }
+  constructor(public usersService: UsersService) {}
 
   ngOnInit(): void {
+    this.usersService.createUser({
+      name: 'morpheus',
+      job: 'leader',
+    });
+    this.usersService.editUser({
+      name: 'morpheus',
+      job: 'zion resident',
+    });
+    this.usersService.getUsers().subscribe((data) => {
+      this.data = data;
+    });
+    this.usersService.getError().subscribe(
+      (data) => {
+        console.log('User Login: ' + data.login);
+        console.log('Bio: ' + data.bio);
+        console.log('Company: ' + data.company);
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('Client-side error');
+        } else {
+          console.log('Server-side error');
+        }
+      }
+    );
   }
-
 }
---
+++ src/app/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) {
+    //this.http.get('https://reqres.in/api/users?page=2');
+  }
+
+  getUsers(): Observable<any> {
+    return this.http.get('https://reqres.in/api/users?page=2');
+  }
+
+  createUser(user: any): Observable<any>{
+    return this.http.post('https://reqres.in/api/users', user);
+  }
+
+  editUser(user: any): Observable<any>{
+    return this.http.post('https://reqres.in/api/users/2', user)
+  }
+
+  deleteUser(): Observable<any>{
+    return this.http.delete('https://reqres.in/api/users/2');
+  }
+
+  getError(): Observable<any>{
+    return this.http.get("https://api.github.com/users/seeschweiler");
+  }
+
+}
